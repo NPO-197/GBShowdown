8722185963b6233c91aebb8972377c809cda2821
NPO <npofred45@gmail.com>
2023-05-10 08:43:52 -0400

    Initial Commit

---
+++ MockUp Stealth Game/assets/backgrounds/Shop+Resturant.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Shop+Resturant" tilewidth="8" tileheight="8" tilecount="182" columns="14">
+ <image source="TileSet Shop+Restaurant.png" width="112" height="104"/>
+</tileset>
